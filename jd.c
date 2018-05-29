56eac555e8ab0e8b8b06d48975ea0cd9deb5ecd9
zxxu <xzx325@163.com>
2018-05-29 18:49:39 +0800

    error

--- jd.c
+++ jd.c
@@ -2,8 +2,8 @@
 float a;
 void main(){
 	a = 0;
-	unsigned char * ptr = &a;
-	int len = sizeof(a);
+	unsigned char * ptr = &a
+	int len = sizeof(a)
 	for(int i=0;i<len;i++){
 		printf("%x ",ptr[i]);
 	}
